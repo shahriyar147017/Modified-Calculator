05b662e4b880475017f1e657ab2b57ffa3359e7e
shahriyar147017 <72671139+shahriyar147017@users.noreply.github.com>
2020-10-11 03:13:51 +0600

    Add files via upload

---
+++ Modified Calculator.py
@@ -0,0 +1,42 @@
+import math
+while True:
+    firstNumber = float(input('Insert your 1st number: '))
+    op = input('Insert your operator: ').lower()
+    secondNumber = float(input('Insert your 2nd number: '))
+
+    if op == "+":
+        print (firstNumber, "+", secondNumber, "=", firstNumber + secondNumber )
+    elif op == "-": 
+        print (firstNumber, "-", secondNumber, "=", firstNumber - secondNumber )
+    elif op == "*":
+        print (firstNumber, "*", secondNumber, "=", firstNumber * secondNumber )
+    elif op == "/":
+        print (firstNumber, "/", secondNumber, "=", firstNumber / secondNumber )
+    elif op == "^":
+        print (firstNumber, "^", secondNumber, "=", firstNumber ** secondNumber )
+    elif op == "r":
+        print (firstNumber, "root", secondNumber, "=", secondNumber ** (1 / firstNumber) )
+    elif op == "%":
+        print (firstNumber, "%", secondNumber, "=", firstNumber % secondNumber )
+    #factorial
+    elif op == "!":
+        theNumber = firstNumber = secondNumber 
+        secondNumber = 1
+        while firstNumber > 1:
+            secondNumber *= firstNumber 
+            firstNumber = firstNumber - 1  
+        print ("n!(", theNumber, ")=", secondNumber )
+    elif op == "sin":
+        print ("sin(", secondNumber, ")=", math.sin(secondNumber ))
+    elif op == "cos":
+        print ("cos(", secondNumber, ")=", math.cos(secondNumber ))
+    elif op == "tan":
+        print ("tan(", secondNumber, ")=", math.tan(secondNumber ))
+    elif op == "pie" or op == "pi":
+        print ("Pie =", math.pi)
+    elif op == "e":
+        print = ("E =", math.e)
+    elif op == "ln":
+        print ("ln(", secondNumber , ")= ", math.log(secondNumber))
+    else:
+        print ("incorrect operator") 
